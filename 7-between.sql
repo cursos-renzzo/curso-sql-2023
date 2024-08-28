6a9cb3423e08c5e256ba88f8149d5e417ca67ae3
Renzzo Rocha <renzzorocha17@gmail.com>
2024-08-28 11:32:15 -0300

    feat: BETWEEN add

---
+++ 7-between.sql
@@ -0,0 +1,8 @@
+SELECT * 
+FROM Production.Product
+WHERE ListPrice NOT BETWEEN 1000 AND 1500
+
+SELECT * 
+FROM HumanResources.Employee
+WHERE HireDate BETWEEN '2009/01/01' AND '2010/01/01'
+ORDER BY HireDate desc
